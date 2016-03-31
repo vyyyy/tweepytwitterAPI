9de972604b16d07d651bfa470c366d64d2413b58
vyyyy <vpat5777@uni.sydney.edu.au>
2016-03-31 16:03:34 +1100

    Update tweepytwitterAPI.py

--- tweepytwitterAPI.py
+++ tweepytwitterAPI.py
@@ -1,30 +1,40 @@
 #! /usr/bin/env python3
+#run script from bash commandline
+#don't forget to setup file permission
 
+#import modules
 import time
 import tweepy
-import os
 import sys
 
-
+#keys for authentication
 consumer_key = X
 consumer_secret = X
 access_token_key = X
 access_token_secret = X
 
-
-class Listener(tweepy.StreamListener):
-
-    def on_data(self, data):
-        print(data)
-        return True
-
-    def on_error(self, status):
-        print(status)
-
-myStreamListener = Listener()
+#connect to Twitter API 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token_key, access_token_secret)
 api = tweepy.API(auth)
+
+#create the Stream Listener class 
+class Listener(tweepy.StreamListener):
+
+    def on_status(self, status):
+        print("New Tweet: ",status.author.screen_name,status.text)
+        return True
+
+    def on_error(self, status_code):
+        if status_code == 420:
+            #returning False disconnects the stream
+            return False
+
+#instantiate myStreamListener object
+myStreamListener = Listener()
+
+#stream will show incoming tweets
 stream = tweepy.Stream(auth, myStreamListener)
 
+#pass commandline argument as a string to filter and track tweets
 stream.filter(track=[sys.argv[1]])
